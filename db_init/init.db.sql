7d9357610da3d738997eba4983016785a0975fad
Marcos <mmaryus1@epsg.upv.es>
2024-10-18 11:38:48 +0200

    Funciona

--- db_init/init.db.sql
+++ db_init/init.db.sql
@@ -1,33 +1,34 @@
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "Gases" (
-	"Nombre"	TEXT NOT NULL UNIQUE,
-	"Medida min"	INTEGER NOT NULL,
-	"Medida max"	INTEGER NOT NULL,
-	"Info"	TEXT,
-	PRIMARY KEY("Nombre")
+CREATE TABLE IF NOT EXISTS `Usuarios` (
+  `ID` INT NOT NULL AUTO_INCREMENT UNIQUE,
+  `Nombre` TEXT,
+  `Contraseña` TEXT,
+  `Correo` TEXT,
+  PRIMARY KEY(`ID`)
 );
-CREATE TABLE IF NOT EXISTS "Medidas" (
-	"ID"	INTEGER NOT NULL,
-	"Gas"	TEXT,
-	"Lugar"	TEXT,
-	"Hora"	NUMERIC,
-	"Valor"	INTEGER,
-	"Sensor"	INTEGER,
-	PRIMARY KEY("ID"),
-	FOREIGN KEY("Gas") REFERENCES "Gases"("Nombre"),
-	FOREIGN KEY("Sensor") REFERENCES "Sensor"("ID")
+
+CREATE TABLE IF NOT EXISTS `Sensor` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Usuario` INT NOT NULL,
+  PRIMARY KEY(`ID`),
+  FOREIGN KEY(`Usuario`) REFERENCES `Usuarios`(`ID`)
 );
-CREATE TABLE IF NOT EXISTS "Sensor" (
-	"ID"	INTEGER NOT NULL,
-	"Usuario"	INTEGER NOT NULL,
-	PRIMARY KEY("ID"),
-	FOREIGN KEY("Usuario") REFERENCES "Usuarios"("ID")
+
+CREATE TABLE IF NOT EXISTS `Gases` (
+  `Nombre` VARCHAR(255) NOT NULL UNIQUE,
+  `Medida min` INT NOT NULL,
+  `Medida max` INT NOT NULL,
+  `Info` TEXT,
+  PRIMARY KEY(`Nombre`)
 );
-CREATE TABLE IF NOT EXISTS "Usuarios" (
-	"ID"	INTEGER NOT NULL UNIQUE,
-	"Nombre"	TEXT,
-	"Contraseña"	TEXT,
-	"Correo"	TEXT,
-	PRIMARY KEY("ID")
+
+CREATE TABLE IF NOT EXISTS `Medidas` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Gas` VARCHAR(255),  -- Usamos VARCHAR para permitir el uso en claves foráneas
+  `Lugar` TEXT,
+  `Hora` DATETIME,
+  `Valor` INT,
+  `Sensor` INT,
+  PRIMARY KEY(`ID`),
+  FOREIGN KEY(`Gas`) REFERENCES `Gases`(`Nombre`),
+  FOREIGN KEY(`Sensor`) REFERENCES `Sensor`(`ID`)
 );
-COMMIT;
